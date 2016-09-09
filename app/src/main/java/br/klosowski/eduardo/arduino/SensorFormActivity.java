e5de2e2f6cec6c68b108a8cdfb99960f27b6e43f
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2016-09-09 19:29:15 -0300

    Lista estática de dispositivos

--- app/src/main/java/br/klosowski/eduardo/arduino/SensorFormActivity.java
+++ app/src/main/java/br/klosowski/eduardo/arduino/SensorFormActivity.java
@@ -2,10 +2,17 @@ package br.klosowski.eduardo.arduino;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatSpinner;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SensorFormActivity extends AppCompatActivity {
+
+    private AppCompatSpinner mSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,18 @@ public class SensorFormActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mSpinner = (AppCompatSpinner) findViewById(R.id.spinner1);
+        List<String> list = new ArrayList<>();
+        list.add("Exemplo Android 1");
+        list.add("Exemplo Android 2");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>
+                (this, android.R.layout.simple_spinner_item, list);
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        mSpinner.setAdapter(dataAdapter);
     }
 
     @Override
